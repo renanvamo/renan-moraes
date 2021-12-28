9b8c9228e6abb67ed095945aa423dcd2f5aae7d9
Renan <renan.vamo@gmail.com>
2021-12-28 12:15:11 -0300

    feat: faz chamada de rotas no App e cria rota inicial "/" que renderiza a Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Meu portfolio
-    </div>
+    <Switch>
+      <Route exact path ="/" component={ Home } />
+    </Switch>
   );
 }
 
