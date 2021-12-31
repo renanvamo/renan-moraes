f2e17e35ed720a7ff427f632bb61ff6393f062b9
Renan <renan.vamo@gmail.com>
2021-12-31 00:42:58 -0300

    style: finaliza estilo da page home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,15 @@ export default function Home() {
   return (
     <>
       <Header />
-      <h1 className="home-title">Renan Moraes</h1>
-      <p>Full Stack Web Developer</p>
-      <p>Working and Studying at Trybe</p>
-      <p>Trying to bring aknowledge and opportunities to people</p>
+      <section className="home-body">
+        <h1 className="home-title">Renan Moraes</h1>
+        <p>Full Stack Web Developer</p>
+        <p>Working and studying at Trybe</p>
+        
+        <blockquote>
+          "I want to make difference, bringing aknowledge and opportunities to people"
+        </blockquote>
+      </section>
       <Footer />
     </>
   );
