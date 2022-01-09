8a4cd0729c4216a026c44671a6e61dc2d49293a2
Renan <renan.vamo@gmail.com>
2022-01-09 01:57:58 -0300

    feat: adiciona um botao para contato pelo whatsapp na pagina de contatos

---
+++ src/components/WhatsApp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function WhatasApp() {
+  return (
+    <div>
+      <a
+        href="https://wa.me/5511963432571"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
+    </div>
+  )
+}
